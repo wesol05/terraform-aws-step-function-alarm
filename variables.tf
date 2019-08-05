d6967d12183231674b9a995bc03d08c7d22e38a1
Tomasz Wesołowski <wesol05@gmail.com>
2019-08-05 10:24:26 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "Name of the alarm."
+}
+
+variable "state-machine-arn" {
+  description = "Arn of the state machine to monitor."
+}
+
+variable "sns-topic" {
+  description = "Name of the topic where to send a notifications."
+}
+
+variable "tags" {
+  type = "map"
+  default = {}
+}
+
+variable "enabled" {
+  default = true
+}
+variable "period" {
+  default = 60
+  description = "The period in seconds over which the specified statistic is applied."
+}
+
+variable "evaluation_periods" {
+  default = 1
+  description = "The number of periods over which data is compared to the specified threshold."
+}
+
+variable "datapoints_to_alarm" {
+  default = 1
+  description = "The number of datapoints that must be breaching to trigger the alarm."
+}
